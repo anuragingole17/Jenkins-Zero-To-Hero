647b217e95757d515585f2a21aed19db0ec4457e
Ubuntu <ubuntu@ip-172-31-92-78.ec2.internal>
2025-08-26 18:01:29 +0000

    java program hello word

---
+++ javaprogram.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+
+
+echo ' 
+  public class Hello {
+
+
+      public static void main(String[] args)
+ 	{
+
+		System.out.println("Hello world");	
+
+
+
+    } 
+ }
+
+' >  Hello.java
+
+
+
+javac Hello.java
+
+
+
+java Hello
